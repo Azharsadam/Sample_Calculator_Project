dbb6ffc4c89ced316fcd2a300e06bb9b68ae354b
EC2 Default User <ec2-user@ip-172-31-82-138.ec2.internal>
2024-03-02 20:11:55 +0000

    this is my first second version

---
+++ calculator.sh
@@ -0,0 +1,9 @@
+def add_numbers(n1, n2):
+    return n1 + n2
+
+n1 = float(input('Enter first number: '))
+n2 = float(input('Enter second number: '))
+result = add_numbers(n1, n2)
+print('=', result)
+
+print("this is my first version")
